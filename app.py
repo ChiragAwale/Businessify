e2394e6774100b6e76c525502b62e56a7a1d11c1
chiragawale <chiragawale777@gmail.com>
2018-11-11 16:26:24 -0600

    Service Layer Added

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
 
 
 if __name__ == '__main__':
     app.run()
+
