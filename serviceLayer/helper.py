0e8bd4b4590e7a3f760b10255bd757a0d75e4e31
chiragawale <chiragawale777@gmail.com>
2018-11-18 20:18:32 -0600

    float conversion for numpy

--- serviceLayer/helper.py
+++ serviceLayer/helper.py
@@ -2,8 +2,12 @@ import json
 import os
 import ijson
 import pandas as pd
+from pandas.io.json import json_normalize
+import numpy as np
+import matplotlib.pyplot as plt
 
 
+dataf = []
 
 def read_json(json_data):
     loaded_json= json.loads(json_data)
@@ -18,8 +22,6 @@ def read_json_from_file(file_name):
         return data
 
 def read_djson():
-
-    dataf = []
     DIRECTORY = os.path.dirname(os.path.abspath(__file__))+'/data/'
 
     with open(DIRECTORY + "yelp_business.json") as f:
@@ -27,9 +29,25 @@ def read_djson():
         for line in f:
             j_content = json.loads(line)
             dataf.append(j_content)
+            print(j_content)
+    df = pd.DataFrame.from_dict(json_normalize(dataf), orient = 'columns')
+    print(list(df.columns.values))
+    print(df["name"].value_counts())
 
-    df = pd.DataFrame.from_dict(dataf, orient = 'columns')
-    print(df["state"].value_counts())
+    def parse_float(x):
+        try:
+            x = float(x)
+        except Exception:
+            x = 0
+        return x
+
+    df["longitude"] = df["longitude"].apply(parse_float)
+    df["latitude"] = df["latitude"].apply(parse_float)
+
+    print(df["longitude"].values)
+
+    plt.hist(df["name"],bins = 100)
 
 
 read_djson()
+
